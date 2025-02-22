f528ec97c13e2892e7472f9f64cee3ca53a64358
marcos lott <lott.marcos@gmail.com>
2025-02-21 22:17:27 -0300

    test: adding beforeAll function to clear database before tests runs

--- tests/integration/api/v1/status/get.test.ts
+++ tests/integration/api/v1/status/get.test.ts
@@ -1,4 +1,10 @@
+import { clearDatabase } from "tests/utils";
+
 describe("GET /api/v1/status", () => {
+  beforeAll(async () => {
+    await clearDatabase();
+  });
+
   it("should GET to /api/v1/status should return 200", async () => {
     const response = await fetch("http://localhost:3000/api/v1/status");
     expect(response.status).toBe(200);
