5eb4004e2bde69523fc49ad35bd86db79079c9e8
marcos lott <lott.marcos@gmail.com>
2025-02-23 17:32:52 -0300

    feat: adding protections for db connections

--- pages/api/v1/migrations/index.ts
+++ pages/api/v1/migrations/index.ts
@@ -4,7 +4,8 @@ import node_pg_migration from "node-pg-migrate";
 import { join } from "node:path";
 
 const migrations = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method !== "GET" && req.method !== "POST") {
+  const alowedMethods = ["GET", "POST"];
+  if (!alowedMethods.includes(req.method)) {
     res.status(405).json({ error: "Method not allowed" });
     return;
   }
@@ -12,20 +13,26 @@ const migrations = async (req: NextApiRequest, res: NextApiResponse) => {
   const dbClient = await getClient();
   const dryRun = req.method === "GET";
 
-  const migrations = await node_pg_migration({
-    dbClient,
-    migrationsTable: "pgmigrations",
-    dir: join("infra", "migrations"),
-    direction: "up",
-    dryRun,
-    verbose: true,
-  });
+  try {
+    const migrations = await node_pg_migration({
+      dbClient,
+      migrationsTable: "pgmigrations",
+      dir: join("infra", "migrations"),
+      direction: "up",
+      dryRun,
+      verbose: true,
+    });
 
-  await dbClient.end();
+    if (migrations.length > 0) return res.status(201).json(migrations);
 
-  if (migrations.length > 0) return res.status(201).json(migrations);
-
-  res.status(200).json(migrations);
+    res.status(200).json(migrations);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+    throw error;
+  } finally {
+    await dbClient.end();
+  }
 };
 
 export default migrations;
